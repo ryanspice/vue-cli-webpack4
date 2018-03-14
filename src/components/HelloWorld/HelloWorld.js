dc68337ba65885adcfa55af0af1c6e9fd6348df3
Ryan Spice <ryan.spice-finnie@openlane.com>
2018-03-13 21:45:23 -0400

    Removing not yet working feature

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,18 +1,16 @@
-/* @flow */
+
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 @Component()
 export default class HelloWorld extends Vue {
 
-	number:string = 'Welcome to Your PLIB Vue.js App';
-
 	/**/
 
 	data() {
 
 		return {
-			msg: this.number,
+			msg: 'Welcome to Your PLIB Vue.js App',
 		};
 
 	}
